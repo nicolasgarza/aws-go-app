514ab99f8038fc02d1fd36f4e550bacb4b794c32
Nicolas Garza <nicolas.garza@gmail.com>
2024-05-21 19:42:57 -0500

    api handler and create table

--- aws-go-app.go
+++ aws-go-app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -18,11 +19,22 @@ func NewAwsGoAppStack(scope constructs.Construct, id string, props *AwsGoAppStac
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	awslambda.NewFunction(stack, jsii.String("MyLambdaFunction"), &awslambda.FunctionProps{
+	// create db table
+	table := awsdynamodb.NewTable(stack, jsii.String("myUserTable"), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("username"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		TableName: jsii.String("userTable"),
+	})
+
+	myFunction := awslambda.NewFunction(stack, jsii.String("MyLambdaFunction"), &awslambda.FunctionProps{
 		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
 		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
 		Handler: jsii.String("main"),
 	})
+
+	table.GrantReadWriteData(myFunction)
 
 	return stack
 }
--- lambda/api/api.go
+++ lambda/api/api.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"fmt"
 	"lambda-func/database"
+	"lambda-func/types"
 )
 
 type ApiHandler struct {
@@ -12,4 +14,30 @@ func NewApiHandler(dbStore database.DynamoDBClient) ApiHandler {
 	return ApiHandler{
 		dbStore: dbStore,
 	}
+}
+
+func (api ApiHandler) RegisterUserHandler(event types.RegisterUser) error {
+	if event.Username == "" || event.Password == "" {
+		return fmt.Errorf("request has empty parameters")
+	}
+
+	// does a user with this username already exist
+
+	userExists, err := api.dbStore.DoesUserExist(event.Username)
+	if err != nil {
+		return fmt.Errorf("there was an error checking if user exists: %w", err)
+	}
+
+	if userExists {
+		return fmt.Errorf("a user with that username already exists")
+	}
+
+	// we know that a user does not exist
+
+	err = api.dbStore.InsertUser(event)
+	if err != nil {
+		return fmt.Errorf("error registering the user %w", err)
+	}
+
+	return nil
 }
